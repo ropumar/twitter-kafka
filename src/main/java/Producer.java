c0ee5c233468916d0fb138422ad15029bd3c330f
Treinamento Tecgraf <treinamento@lab008.tecgraf.puc-rio.br>
2019-05-28 11:25:28 -0300

    final producer com serializer

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -5,23 +5,21 @@ import java.time.LocalDate;
 import java.util.Properties;
 
 public class Producer {
-    private KafkaProducer<String, String> producer;
+    private KafkaProducer<String, Tweet> producer;
     private ProducerRecord record;
     public Producer() {
         // Criar as propriedades do produtor
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, TweetSerializer.class.getName());
 
         // Criar o produtor
-        producer = new KafkaProducer<String, String>(properties);
+        producer = new KafkaProducer<String, Tweet>(properties);
     }
     public void ProducerSendMessage(String topic, Tweet tweet){
         // Enviar as mensagens
-
-
-        record = new ProducerRecord<String, String>(topic, tweet.getMessage());
+        record = new ProducerRecord<String, Tweet>(topic, tweet);
         producer.send(record); // Envio assíncrono
     }
     public void ProducerClose() {
